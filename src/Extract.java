1cb2bf2ef719a8306511c5db15e49a15c6cefc41
Michael <51711533+Ganondorc@users.noreply.github.com>
2023-06-07 09:23:59 -0600

    cleaned up logic in filtering stage

--- src/Extract.java
+++ src/Extract.java
@@ -162,6 +162,19 @@ public class Extract {
 
 	}
 
+	private List<String> supplies = Arrays.asList("FAMILY DOLLAR", "WALMART", "WalMart", "WALGREENS", "KROGER", "Club", "CLUB", "DYNAREX", "GLOVE");
+	private List<String> maintenance = Arrays.asList("DEPOT", "LOWE", "THE HOME DE", "HARBOR FREI");
+	private List<String> meals = Arrays.asList("WAITR", "BJ's", "CAESARS", "CHICK", "SOMBRERO", "FOOD", "JUCYS", "BUCEES", "CAFE", "PIZZA", "STARBUCKS", "TACO", "ROADHOUSE", "BUTCHER", "THE CATCH", "BURGER", "YANG", "SONIC", "ROTOLOS", "MCDONALDS", "CASA OLE", "BAR", "GRILL", "CANES", "OUTBACK", "PANDA", "DONUT", "BROOKSHIR", "SUPER F");
+	private List<String> costOfGoodsSold = Arrays.asList("METAL MAFIA", "NEILMED", "NEOMETAL", "BODY JEWELRY", "HOLLYWOOD BODY", "GEMS", "KINGPIN", "JUNIPURR", "ANATOMETAL", "HENRY SCHEIN", "INDUSTRIAL STR", "BODY VISION");
+	private List<String> employeeBenefitsPrograms = Arrays.asList("DILLARD", "Dillards", "OIL BOWL", "UNIMAX", "WASH MASTER", "PETSMART", "BOUTIQUE", "BED BATH");
+	private List<String> officeExpenses = Arrays.asList("HOBBYLOBBY", "MICHAELS");
+	private List<String> insuranceAndMedical = Arrays.asList("CHRISTUS", "Aid");
+	private List<String> travel = Arrays.asList("HOTWIRE", "RENTACAR", "RENT A CAR", "Ticket", "AIRLINES", "SHELL", "VACATION", "WINGATE", "ZIPPY JS", "FUEL", "EXXON", "EXPRESS", "INN", "SUITE", "HOTEL", "CHEVRON", "DFW", "EZ MART", "PRICELINE", "SAMS TOWN", "KYLES", "TIGER MART", "LYFT", "RIDE", "AIRBNB");
+	private List<String> taxesAndLicenses = Arrays.asList("TAX", "Charge", "ASSOCIATION OF PROFESS");
+	private List<String> rentOrLease = Arrays.asList("STORAGE", "SAFEBOX");
+	private List<String> utilities = Arrays.asList("802", "Regency", "Fugis", "CABLE", "UTILITY", "ELECT");
+	private List<String> communication = Arrays.asList("STRAIGHTTALK", "VONAGE");
+
 	static String outputTaxCSV(File inputFile) {
 		String text = null;
 		try {
@@ -271,93 +284,34 @@ public class Extract {
 				paymentType = "ACH";
 
 			String category = "";
-			if (vendor.contains("FAMILY DOLLAR") || vendor.contains("WALMART") || vendor.contains("WalMart")
-					|| vendor.contains("WALGREENS") || vendor.contains("KROGER") || vendor.contains("Club")
-					|| vendor.contains("CLUB") || vendor.contains("DYNAREX") || vendor.contains("GLOVE")) {
+			if (supplies.stream().anyMatch(vendor::contains)) {
 				category = "Supplies";
-			} else if (vendor.contains("DEPOT") || vendor.contains("LOWE") || vendor.contains("THE HOME DE")
-					|| vendor.contains("HARBOR FREI")) {
+			} else if (maintenance.stream().anyMatch(vendor::contains)) {
 				category = "Maintenance";
-			} else if (vendor.contains("WAITR") || vendor.contains("BJ's") || vendor.contains("CAESARS")
-					|| vendor.contains("CHICK") || vendor.contains("SOMBRERO") || vendor.contains("FOOD")
-					|| vendor.contains("JUCYS") || vendor.contains("BUCEES") || vendor.contains("CAFE")
-					|| vendor.contains("PIZZA") || vendor.contains("STARBUCKS") || vendor.contains("TACO")
-					|| vendor.contains("ROADHOUSE") || vendor.contains("BUTCHER") || vendor.contains("THE CATCH")
-					|| vendor.contains("BURGER") || vendor.contains("YANG") || vendor.contains("SONIC")
-					|| vendor.contains("ROTOLOS") || vendor.contains("MCDONALDS") || vendor.contains("CASA OLE")
-					|| vendor.contains("BAR") || vendor.contains("GRILL") || vendor.contains("CANES")
-					|| vendor.contains("OUTBACK") || vendor.contains("PANDA") || vendor.contains("DONUT")
-					|| vendor.contains("BROOKSHIR") || vendor.contains("SUPER F")) {
+			} else if (meals.stream().anyMatch(vendor::contains)) {
 				category = "Meals";
-			} else if (vendor.contains("METAL MAFIA") || vendor.contains("NEILMED") || vendor.contains("NEOMETAL")
-					|| vendor.contains("BODY JEWELRY") || vendor.contains("HOLLYWOOD BODY") || vendor.contains("GEMS")
-					|| vendor.contains("KINGPIN") || vendor.contains("JUNIPURR") || vendor.contains("ANATOMETAL")
-					|| vendor.contains("HENRY SCHEIN") || vendor.contains("INDUSTRIAL STR")
-					|| vendor.contains("BODY VISION")) {
+			} else if (costOfGoodsSold.stream().anyMatch(vendor::contains)) {
 				category = "Cost of goods sold";
-			} else if (vendor.contains("DILLARD") || vendor.contains("Dillards") || vendor.contains("OIL BOWL")
-					|| vendor.contains("UNIMAX") || vendor.contains("WASH MASTER") || vendor.contains("PETSMART")
-					|| vendor.contains("BOUTIQUE") || vendor.contains("BED BATH")) {
+			} else if (employeeBenefitsPrograms.stream().anyMatch(vendor::contains)) {
 				category = "Employee benefits programs";
-			} else if (vendor.contains("HOBBYLOBBY") || vendor.contains("MICHAELS")) {
+			} else if (officeExpenses.stream().anyMatch(vendor::contains)) {
 				category = "Office expenses";
-			} else if (vendor.contains("CHRISTUS") || vendor.contains("Aid")) {
+			} else if (insuranceAndMedical.stream().anyMatch(vendor::contains)) {
 				category = "Insurance and medical";
-			} else if (vendor.contains("HOTWIRE") || vendor.contains("RENTACAR") || vendor.contains("RENT A CAR")
-					|| vendor.contains("Ticket") || vendor.contains("AIRLINES") || vendor.contains("SHELL")
-					|| vendor.contains("VACATION") || vendor.contains("WINGATE") || vendor.contains("ZIPPY JS")
-					|| vendor.contains("FUEL") || vendor.contains("EXXON") || vendor.contains("EXPRESS")
-					|| vendor.contains("INN") || vendor.contains("SUITE") || vendor.contains("HOTEL")
-					|| vendor.contains("CHEVRON") || vendor.contains("DFW") || vendor.contains("EZ MART")
-					|| vendor.contains("PRICELINE") || vendor.contains("SAMS TOWN") || vendor.contains("KYLES")
-					|| vendor.contains("TIGER MART") || vendor.contains("LYFT") || vendor.contains("RIDE")
-					|| vendor.contains("AIRBNB")) {
+			} else if (travel.stream().anyMatch(vendor::contains)) {
 				category = "Travel";
-			} else if (vendor.contains("TAX") || vendor.contains("Charge")
-					|| vendor.contains("ASSOCIATION OF PROFESS")) {
+			} else if (taxesAndLicenses.stream().anyMatch(vendor::contains)) {
 				category = "Taxes and licenses";
-			} else if (vendor.contains("STORAGE") || vendor.contains("SAFEBOX")) {
+			} else if (rentOrLease.stream().anyMatch(vendor::contains)) {
 				category = "Rent or lease";
-			} else if (vendor.contains("BP")) {
-				if (vendor.contains("802") || vendor.contains("Regency")) {
-					if (vendor.contains("Mort")) {
-						vendor = "802 Regency Mortage";
-						category = "Mortgage";
-					} else if (vendor.contains("Cit")) {
-						vendor = "802 Regency City of Longview Water";
-						category = "Utilities";
-					} else if (vendor.contains("Cen")) {
-						vendor = "802 Regency Center Point Energy";
-						category = "Utilities";
-					} else if (vendor.contains("Lon")) {
-						vendor = "802 Regency Longview Cable";
-						category = "Utilities";
-					} else if (vendor.contains("SWE")) {
-						vendor = "802 Regency SWEPCO";
-						category = "Utilities";
-					}
-				} else if (vendor.contains("Fugis")) {
-					if (vendor.contains("City")) {
-						vendor = "Fugi's City of Longview Water";
-						category = "Utilities";
-					} else if (vendor.contains("ATT")) {
-						vendor = "Fugi's AT&T";
-						category = "Utilities";
-					} else if (vendor.contains("SWEPCO")) {
-						vendor = "Fugi's SWEPCO";
-						category = "Utilities";
-					}
-				} else if (vendor.contains("Amazon")) {
-					vendor = "Amazon";
-					category = "Supplies";
-				}
-			} else if (vendor.contains("CABLE") || vendor.contains("UTILITY") || vendor.contains("ELECT")) {
+			} else if (utilities.stream().anyMatch(vendor::contains)) {
 				category = "Utilities";
-			} else if (vendor.contains("STRAIGHTTALK") || vendor.contains("VONAGE")) {
+			} else if (communication.stream().anyMatch(vendor::contains)) {
 				category = "Communication";
 			} else {
 				category = "Other expenses";
 			}
+			
 			vendor = StringUtilities.toCamelCase(vendor);
 			output += (date + "," + vendor + "," + category + "," + paymentType + "," + amount + ",Imported from "
 					+ outputFileName.substring(0, outputFileName.indexOf(".")) + " statement") + "\n";
